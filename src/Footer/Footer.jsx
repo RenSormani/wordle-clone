658c5d709d053952591d0b26e8f143afbee2611c
Renato Sormani <ilgiardino@hotmail.co.uk>
2022-10-05 11:34:45 +0100

    Added all files

---
+++ src/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="footer-text">
+        <p>
+            Click on the word 
+            <a href="https://www.nytimes.com/games/wordle/index.html"> "Wordle"</a>, to head over to the original <a href="https://www.nytimes.com/games/wordle/index.html"> "Wordle"</a> - the inspiration for my <a href="https://www.nytimes.com/games/wordle/index.html"> "Wordle"</a>. Now, stop saying <a href="https://www.nytimes.com/games/wordle/index.html"> "Wordle"</a>...
+        </p>
+        </div>
+  )
+}
+
+export default Footer
---
+++ src/components/GameOver/GameOver.jsx
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../../App';
+
+const GameOver = () => {
+    const {
+        board,
+        setBoard,
+        currAttempt,
+        gameOver,
+        onSelectLetter,
+        correctWord,
+        onDelete,
+      } = useContext(AppContext);
+      return (
+        <div className="gameOver">
+          <h3>
+            {gameOver.guessedWord
+              ? "You Correctly Guessed the Renato-dle"
+              : "Wahhhh. You failed to guess the word"}
+          </h3>
+          <h1>Correct Word: {correctWord}</h1>
+          {gameOver.guessedWord && (
+            <h3>You guessed in {currAttempt.attempt} attempts</h3>
+          )}
+        </div>
+      );
+    }
+
+export default GameOver
